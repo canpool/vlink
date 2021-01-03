172da387c15fe209f4b3bc030b455f7adf7a5723
canpool <canpool@163.com>
2021-01-03 20:15:18 +0800

    Add vconfig.h Modules:base Defect:AR.SR.IRyymm00.000.000

---
+++ include/vconfig.h
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) [2021] vlink Team. All rights reserved.
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#ifndef __VCONFIG_H__
+#define __VCONFIG_H__
+
+#include <kconfig.h>
+
+#endif /* __VCONFIG_H__ */
