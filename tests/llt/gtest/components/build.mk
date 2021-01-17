d4a42f7fa06f8278b027370565bd1f7410ab2000
canpool <canpool@163.com>
2021-01-17 15:35:46 +0800

    Adjust the test directory of components Modules:gtest Defect:AR.SR.IR200704.001.000
    move some simple test-case to other-dir

--- tests/llt/gtest/components/build.mk
+++ tests/llt/gtest/components/build.mk
@@ -1,4 +1,1 @@
-src-prefix              := src
-src-$(CONFIG_VLOG)      += test_vlog.cpp
-src-$(CONFIG_SECUREC)   += test_securec.cpp
-src-$(CONFIG_CRC)       += test_crc.cpp
+dir-y = *
---
+++ tests/llt/gtest/components/other/build.mk
@@ -0,0 +1,4 @@
+src-prefix              := src
+src-$(CONFIG_VLOG)      += test_vlog.cpp
+src-$(CONFIG_SECUREC)   += test_securec.cpp
+src-$(CONFIG_CRC)       += test_crc.cpp
